3b8f34e47e9e2971fde5f122993c4cf899d50920
zsdghlk <hrdsjsj@gmail.com>
2025-09-06 22:46:14 +0000

    feat: first push from Codespaces

---
+++ node_modules/twitter-api-v2/dist/cjs/types/v2/community.v2.types.d.ts
@@ -0,0 +1,36 @@
+export declare type CommunityJoinPolicy = 'Open' | 'RestrictedJoinRequestsDisabled' | 'RestrictedJoinRequestsRequireAdminApproval' | 'RestrictedJoinRequestsRequireModeratorApproval' | 'SuperFollowRequired';
+export interface CommunityV2 {
+    id: string;
+    name: string;
+    created_at: string;
+    description?: string;
+    access?: 'Public' | 'Closed';
+    join_policy?: CommunityJoinPolicy;
+    member_count?: number;
+}
+export interface CommunityErrorV2 {
+    title: string;
+    type: string;
+    detail?: string;
+    status?: number;
+}
+export interface CommunityV2Result {
+    data: CommunityV2;
+    errors?: CommunityErrorV2[];
+}
+export interface CommunitiesV2Result {
+    data: CommunityV2[];
+    errors?: CommunityErrorV2[];
+    meta: {
+        next_token?: string;
+    };
+}
+export interface CommunityByIDV2Params {
+    id: string;
+}
+export interface CommunitySearchV2Params {
+    query: string;
+    max_results?: number;
+    next_token?: string;
+    pagination_token?: string;
+}
